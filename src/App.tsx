528072c16cd38ee8fcb8d5708b0f05e25b4bf673
penge <ppenge@gmail.com>
2020-08-14 17:00:06 +0200

    Initialize project using Create React App
    - Files are generated using $ npx create-react-app my-app --template typescript
    
    - Description for Expenses Tracker is already set (package.json and index.html)
    
    - Unnecessary boilerplate files are removed (serviceWorker.ts, favicon.icon, logo192.png, logo512.png, manifest.json), index.html and App.tsx are simplified from what is generated

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Expenses Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
