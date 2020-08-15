0931fcbcd37832ebebf72bd8776e05312d0f9bb4
penge <ppenge@gmail.com>
2020-08-15 13:15:48 +0200

    Define PrivateRoute

---
+++ src/routes/PrivateRoute.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const PrivateRoute: React.FC<{
+  component: React.FC;
+  path: string;
+  exact: boolean;
+}> = (props) => {
+  const email = localStorage.getItem("email");
+
+  // A dummy check of a succesfull login
+  const isAuthenticated = typeof email === "string" &&
+    email.length > 0 &&
+    email.indexOf("@") > 0;
+
+  return isAuthenticated ? (<Route path={props.path} exact={props.exact} component={props.component} />) :
+    (<Redirect to={{ pathname: "/login", state: { from: props.path }}} />); // not authenticated
+};
+
+export default PrivateRoute;
